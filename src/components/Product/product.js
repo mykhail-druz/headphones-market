810fb297f7fcf676dfaa978d0bef8e512becbd26
Михаил <dda3127@gmail.com>
2023-01-10 17:15:53 +0200

    Fixed rendering issue

--- src/components/Product/product.js
+++ src/components/Product/product.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
 import { urlFor } from "~/lib/client";
-import Button from "~/components/Button/button";
 
 const Product = ({
   product: {
@@ -16,16 +15,12 @@ const Product = ({
             src={urlFor(image && image[0])}
             width={250}
             height={250}
-            className="product-image"
-          />
+            className="product-image"/>
         )}
         <div className="d-flex">
           <div className="m-3">
             <p className="product-name">{name}</p>
             <p className="product-price">${price}</p>
-          </div>
-          <div className="tw-ml-auto tw-mt-auto tw-mb-6">
-            <Button link={`/product/${slug.current}`}>Buy</Button>
           </div>
         </div>
       </div>
