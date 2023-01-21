c23709258cfec2e3e14775bec158808cd488bd8a
DoveK <d.kachanko@gmail.com>
2023-01-22 00:55:19 +0100

    Changes in banner, rebase

--- src/components/Header/banner.js
+++ src/components/Header/banner.js
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import React, { useState, useEffect } from "react";
 import { urlFor } from "~/lib/client";
 import Button from "../Button/button";
@@ -14,39 +15,63 @@ function Banner({ banner }) {
 
     return () => clearInterval(intervalId);
   }, []);
-
   return (
-    <div className="md:h-[50vh] w-full border bg-white ">
-      <div className="flex justify-between flex-col-reverse md:flex-row">
-        <div className="flex flex-col p-4 m-4 w-[325px] md:justify-start justify-center">
+    <div className="flex flex-col">
+      <div className="md:h-[60vh] w-full border bg-white">
+        <div className="flex justify-between flex-col-reverse md:flex-row w-8/12 mx-auto h-full items-center">
+          <div className="flex flex-col p-4 m-4 w-[325px] md:justify-start justify-center text-center md:text-left">
+            <h1 className="text-3xl py-1">
+              {banner[currentBannerIndex].largeText1}
+            </h1>
+            <h1 className="text-3xl py-1">
+              {banner[currentBannerIndex].largeText2}
+            </h1>
+            <p className="py-1">{banner[currentBannerIndex].desc}</p>
+            <Button
+              link={`/product/${banner[currentBannerIndex].slug.current}`}
+              className="w-48 my-1 border-2 cursor-pointer text-bold lg:mx-0 mx-auto"
+            >
+              {banner[currentBannerIndex].buttonText}
+            </Button>
+          </div>
           <div className="">
-          <h1 className="text-3xl py-1">
-            {banner[currentBannerIndex].largeText1}
-          </h1>
-          <h1 className="text-3xl py-1">
-            {banner[currentBannerIndex].largeText2}
-          </h1>
-          <p className="py-1">{banner[currentBannerIndex].desc}</p>
-          <Button
-            className="w-48 my-1 text-black bg-secondary hover:bg-black hover:text-white
-          border-2 cursor-pointer text-bold">
-            {banner[currentBannerIndex].buttonText}
-          </Button>
+            <div className="text-center mx-9">
+              {banner[currentBannerIndex].image && (
+                <img
+                  src={urlFor(
+                    banner[currentBannerIndex].image &&
+                      banner[currentBannerIndex].image
+                  ).url()}
+                  className="max-w-[375px] max-h-[375px]"
+                  alt="banner-image"
+                />
+              )}
+              <h2 className="text-3xl ">
+                {banner[currentBannerIndex].product}
+              </h2>
+            </div>
           </div>
         </div>
-        <div className="">
-          <div className="text-center mx-9">
-            {banner[currentBannerIndex].image && (
-              <img
-                src={urlFor(
-                  banner[currentBannerIndex].image &&
-                banner[currentBannerIndex].image).url()}
-                className="w-[375px] h-[375px]"
-                alt="banner-image"/>
-            )}
-            <h2 className="text-3xl ">{banner[currentBannerIndex].product}</h2>
-          </div>
-        </div>
+      </div>
+      <div>
+        <span
+          className={classNames(
+            "rounded-full w-3 h-3 bg-gray-light bg-opacity-90",
+            currentBannerIndex ? "bg-opacity-100" : ""
+          )}
+        ></span>
+        <span
+          className={classNames(
+            "rounded-full w-3 h-3 bg-gray-light bg-opacity-90",
+            currentBannerIndex ? "bg-opacity-100" : ""
+          )}
+        ></span>
+        <span
+          className={classNames(
+            "rounded-full w-3 h-3 bg-gray-light bg-opacity-90",
+            currentBannerIndex ? "bg-opacity-100" : ""
+          )}
+        ></span>
       </div>
     </div>
   );
