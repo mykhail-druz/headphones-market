92bf81a2266ee5c68f66fc371ee738b54c5c82b8
DoveK <46112763+DaniloKachanko@users.noreply.github.com>
2023-01-23 16:47:18 +0100

    Delete node_modules directory

--- node_modules/next-sanity/src/studio/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './NextStudio'
-export * from './NextStudioClientOnly'
-export * from './NextStudioLayout'
-export * from './NextStudioNoScript'
-export * from './usePrefersColorScheme'
-export * from './useTheme'
